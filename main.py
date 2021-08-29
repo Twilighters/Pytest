b1988e3de4734fdc2a3977cdda957c541b41b9cb
Twilighters <ilya475@rambler.ru>
2021-08-29 20:03:34 +0300

    Complete pytest homework

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("qwe")
-print("asd")
-print("qweasd")
+import re
+
+
+def email_check(email: str) -> bool:
+    """Функция проверяющая email."""
+    pattern_re = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+    pattern = re.compile(pattern_re)
+    if not re.match(pattern, email):
+        result = False
+    elif not pattern_re:
+        result = False
+    else:
+        result = True
+    return result
